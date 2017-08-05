973238f2a995b9afa0c739fd8ff2831188331bb3
unknown <mdsardar.javadeveloper@gmail.com>
2017-08-05 08:59:59 +0530

    Initial commit

---
+++ StudentNameEditor.java
@@ -0,0 +1,21 @@
+package com.gontuseries.studentadmissioncontroller;
+
+import java.beans.PropertyEditorSupport;
+
+public class StudentNameEditor extends PropertyEditorSupport {
+
+	@Override
+	public void setAsText(String studentName) throws IllegalArgumentException {
+		
+		if(studentName.contains("Mr.") || (studentName.contains("Ms.")) ) {
+			setValue(studentName);
+		}
+		else
+		{
+			studentName = "Ms. "+studentName;
+			setValue(studentName);
+		}
+		
+	}	
+
+}
